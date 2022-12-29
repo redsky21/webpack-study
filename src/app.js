3b7a2b871a6e0d2b3d383f048750accf0bff9571
sweetoxju@gmail.com <sweetoxju@gmail.com>
2022-12-29 10:33:18 +0900

    jm3

--- src/app.js
+++ src/app.js
@@ -4,3 +4,6 @@ import nyancat from './nyancat.jpg';
 document.addEventListener('DOMContentLoaded',()=>{
     document.body.innerHTML = `<img src="${nyancat}" />`
 })
+console.log(process.env.NODE_ENV);
+console.log(TWO);
+console.log(api.domain);
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
 const path = require('path');
+const webpack = require('webpack');
+const childProcess = require('child_process');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MinCssExtreactPlugin = require('mini-css-extract-plugin');
 module.exports =
     {
         mode: "development",
@@ -23,12 +27,32 @@ module.exports =
                     test: /\.(jpg|png)$/, //로더가 처리해야할 패턴 정규표현식
                     loader:'url-loader',
                     options: {
-                        publicPath: './dist/',
+                        // publicPath: './dist/',
                         name: '[name].[ext]?[hash]',
                         limit: 20000
                     },
 
                 }
             ]
-        }
+        },
+        plugins: [
+            new webpack.BannerPlugin({
+                banner: `Build Date: ${new Date().toLocaleDateString()}
+                Commit Version: ${childProcess.execSync('git rev-parse --short HEAD')}
+                Commit Version: ${childProcess.execSync('git config user.name')}
+                `
+            }),
+            new webpack.DefinePlugin({
+                TWO  : 1+1,
+                'api.domain': JSON.stringify("http://naver.com")
+            }),
+            new HtmlWebpackPlugin({
+                template: "./src/index.html",
+                templateParameters: {
+                    env: process.env.NODE_ENV === 'development' ? '(개발용)': ''
+}
+            }),
+            process.env.NODE_ENV === 'production'?
+            new MinCssExtreactPlugin({filename: '[name].css'}):false
+        ]
     }
